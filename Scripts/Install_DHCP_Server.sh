4adbae394af29d96edbdbe51cc3bd79ddcd41890
James Radtke <cloudxabide@gmail.com>
2023-11-21 12:32:28 -0500

    generic update

--- Scripts/Install_DHCP_Server.sh
+++ Scripts/Install_DHCP_Server.sh
@@ -8,8 +8,8 @@ NEEDRESTART_MODE=a
 sudo apt install -y isc-dhcp-server
 
 cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.orig
-curl https://raw.githubusercontent.com/cloudxabide/kubernerdes.lab/main/Files/etc_dhcp_dhcpd.conf | sudo tee /etc/dhcp/dhcpd.conf
-curl https://raw.githubusercontent.com/cloudxabide/kubernerdes.lab/main/Files/etc_dhcp_dhcpd-hosts.conf | sudo tee /etc/dhcp/dhcpd-hosts.conf
+curl https://raw.githubusercontent.com/cloudxabide/kubernerdes/main/Files/etc_dhcp_dhcpd.conf | sudo tee /etc/dhcp/dhcpd.conf
+curl https://raw.githubusercontent.com/cloudxabide/kubernerdes/main/Files/etc_dhcp_dhcpd-hosts.conf | sudo tee /etc/dhcp/dhcpd-hosts.conf
 systemctl restart isc-dhcp-server.service
 
 
