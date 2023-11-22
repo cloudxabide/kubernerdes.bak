c9f2e3be6b454f6942cb2c4f3f7d8739379b2061
James Radtke <cloudxabide@gmail.com>
2023-11-21 20:20:54 -0500

    generic update

--- Scripts/Install_BIND9.sh
+++ Scripts/Install_BIND9.sh
@@ -22,14 +22,14 @@ do
   curl https://raw.githubusercontent.com/cloudxabide/kubernerdes/main/Files/etc_bind_zones_db.$ZONE.10.10.in-addr.arpa | sudo tee /etc/bind/zones/db.$ZONE.10.10.in-addr.arpa
 done 
 curl https://raw.githubusercontent.com/cloudxabide/kubernerdes/main/Files/etc_bind_zones_db.kubernerdes.lab | sudo tee /etc/bind/zones/db.kubernerdes.lab
-sudo systemctl restart bind9  # You now have a caching nameserver using Google DNS as forwarders
+sudo systemctl restart bind9  # You now have a nameserver for your subnet(s)
 
 cd /etc/bind/zones
 named-checkzone kubernerdes.lab db.kubernerdes.lab
 for FILE in `ls *arpa`; do named-checkzone $(echo $FILE | sed 's/db.//g'; ) $FILE; done
 cd -
 
-sudo systemctl restart systemd-resolved.serviceb
+sudo systemctl restart systemd-resolved.service
 
 exit 0
 
