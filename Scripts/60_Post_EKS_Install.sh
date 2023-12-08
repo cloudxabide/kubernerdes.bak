714eaf346772d36833d03fdf740cab6a3888d0eb
cloudXabide <cloudxabide@gmail.com>
2023-12-08 16:58:55 -0500

    generic update

---
+++ Scripts/60_Post_EKS_Install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+## Enable Metrics Server
+
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl get deployment metrics-server -n kube-system
