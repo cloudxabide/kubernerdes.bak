b4837a15263cd204d93330250eaa7ebfe7daf6db
jaradtke-aws <jaradtke@amazon.com>
2023-12-08 17:09:27 -0500

    Generic Update

--- Scripts/60_Post_EKS_Install.sh
+++ Scripts/60_Post_EKS_Install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+CLUSTER_NAME=eksa
+KUBECONFIG=$(find ~/DevOps/$CLUSTER_NAME -name '*-cluster.kubeconfig')
+
+## Check Cluster Status
+kubectl get pod -A -l control-plane=controller-manager
+kubectl get kubeadmcontrolplanes.controlplane.cluster.x-k8s.io -n eksa-system
+kubectl get clusters.cluster.x-k8s.io -A -o=custom-columns=NAME:.metadata.name,CONTROLPLANE-READY:.status.controlPlaneReady,INFRASTRUCTURE-READY:.status.infrastructureReady,MANAGED-EXTERNAL-ETCD-INITIALIZED:.status.managedExternalEtcdInitialized,MANAGED-EXTERNAL-ETCD-READY:.status.managedExternalEtcdReady
+
 
 ## Enable Metrics Server
 
