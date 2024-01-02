6b54d124e114fc5d6b80c276b0a984ccf43378ec
James Radtke <emailaddy@gmail.com>
2024-01-02 14:56:37 -0500

    generic update

--- Scripts/65_Install_OpenEBS.sh
+++ Scripts/65_Install_OpenEBS.sh
@@ -3,24 +3,24 @@
 #     Purpose:
 #        Date:
 #      Status: Incomplete/In-Progress
-# Assumptions: This script assumes that you have a dummy/disposable cluster, created using 3-nodes which 
-#                are both control-plane and worker nodes (basically my lab setup).
-#        Note: I separate out some of the commands in their own stanza (i.e. I run a for-loop to install, then 
-#                another for-loop to check status).  This allows me to cut-and-paste sections of code while 
-#                I am testing.  (in case my code looks ineffecient ;-)
+# Assumptions: This script assumes that you have a dummy/disposable cluster, created using 3-nodes 
+#                which are both control-plane and worker nodes (basically my lab setup).
+#        Note: I separate out some of the commands in their own stanza (i.e. I run a for-loop to 
+#                install, then another for-loop to check status).  This allows me to 
+#                cut-and-paste sections of code while I am testing.  (in case my code 
+#                looks ineffecient ;-)
 
-HOSTS="eks-host01
-eks-host02
-eks-host03"
+HOSTS="eks-host01 eks-host02 eks-host03"
 
 # Install the iSCSI components on the worker nodes
-# NOTE:  If openEBS was something to be used in "production", you would want to include these steps in your host build
-#          that would be used to deploy your worker nodes.
+# NOTE:  If openEBS was something to be used in "production", you would want to include these 
+#          steps in your host build that would be used to deploy your worker nodes.
 
 # Clean up the SSH Keys (note: this is REALLY not a secure practice - this is just for my lab)
 for HOST in $HOSTS
 do
   ssh-keygen -f "/home/mansible/.ssh/known_hosts" -R "$HOST"
+  ssh-keygen -f "/home/mansible/.ssh/known_hosts.kubernerdes.lab" -R "$HOST"
   ssh -oStrictHostKeyChecking=accept-new -i ~/.ssh/id_ecdsa-kubernerdes.lab ec2-user@$HOST "uptime"
 done
 
@@ -38,6 +38,7 @@ done
 # Check status of iSCSI
 for HOST in $HOSTS
 do
+  echo "#################################################"
   ssh -i ~/.ssh/id_ecdsa-kubernerdes.lab ec2-user@$HOST "
     uname -n
     sudo systemctl status iscsid"
@@ -62,16 +63,22 @@ echo "Disk: $EBS_DEVICE"
 echo "Partition: $EBS_DEVICE_PARTITION"
 
 # Wipe the Disk (THIS IS DESTRUCTIVE - like, for real)
-# NOTE: This needs some cleanup still
-# First - wipe the device
+# NOTE: This needs some cleanup still - and to be clear, this section exists because 
+#       I reuse the lab resources to deploy my cluster repeatedly.
 for HOST in $HOSTS
 do
   ssh -i ~/.ssh/id_ecdsa-kubernerdes.lab ec2-user@$HOST "
     sudo umount /var/openebs
-    sudo lvremove /dev/mapper/vg_localstorage-lv_openebs
+    sudo lvremove --force /dev/mapper/vg_localstorage-lv_openebs
     sudo vgremove --force vg_localstorage
     sudo pvremove --force ${EBS_DEVICE_PARTITION}
     sudo wipefs -a $EBS_DEVICE" 
+done
+
+# Review devices - they should have no partitions now
+for HOST in $HOSTS
+do
+  ssh -i ~/.ssh/id_ecdsa-kubernerdes.lab ec2-user@$HOST "lsblk"
 done
 
 for HOST in $HOSTS
@@ -96,12 +103,13 @@ do
     uname -n 
     sudo vgs
     sudo pvs
+    echo 
     lsblk
+    echo 
     df -h | grep openebs"
   echo
 done
 
-## 
 ##
 # Install OpenEBS via helm
 ##
@@ -114,7 +122,6 @@ helm upgrade openebs openebs/openebs \
 --create-namespace \
 --set jiva.enabled=true
 
-kubectl get ns
 kubectl get pods -n openebs
 
 # Make openEBS your default storage class
@@ -127,7 +134,8 @@ kubectl create namespace openebstest
 kubectl config set-context --current --namespace=openebstest
 curl -o busybox_example_app_persisent_storage.yaml https://raw.githubusercontent.com/cloudxabide/kubernerdes/main/Files/busybox_example_app_persisent_storage.yaml
 kubectl apply -f busybox_example_app_persisent_storage.yaml
-kubectl get pods -n openebstest -w
+# Watch the pods until the busybox pod is "Running", then exit
+while sleep 1; do kubectl get pods -n openebstest | grep Running && break ; done
 
 # Review hosts for new disk image file
 for HOST in $HOSTS
@@ -139,5 +147,14 @@ done
 
 # Clean up app
 ## ADD SECTION FOR REMOVING THE APP
+kubectl delete namespace openebstest
+
+# And check again for the storage block device images
+for HOST in $HOSTS
+do
+  ssh -i ~/.ssh/id_ecdsa-kubernerdes.lab ec2-user@$HOST "
+    sudo iscsiadm -m session -o show
+    find  /var/openebs/local -name 'volume-head*.img' -exec ls -lh {} \; "
+done
 
 exit 0
