1fb11a45513191a70389f4b292e841c90e48b04b
cloudxabide <cloudxabide@gmail.com>
2024-02-11 09:15:01 -0500

    generic update

--- Scripts/11_Install_EKS_Tools.sh
+++ Scripts/11_Install_EKS_Tools.sh
@@ -11,13 +11,15 @@ $(which yq) || sudo snap install yq
 # install the AWS CLI
 sudo snap install aws-cli --classic
 
+# Install eksctl CLI
 curl "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" \
     --silent --location \
     | tar xz -C /tmp
 sudo install -m 0755 /tmp/eksctl /usr/local/bin/eksctl
 
+# Install eksctl-anywhere CLI
 RELEASE_VERSION=$(curl https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml --silent --location | yq ".spec.latestVersion")
-RELEASE_VERSION=v0.18.3 # You can manually set the version also
+#RELEASE_VERSION=v0.18.3 # You can manually set the version also
 EKS_ANYWHERE_TARBALL_URL=$(curl https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml --silent --location | yq ".spec.releases[] | select(.version==\"$RELEASE_VERSION\").eksABinary.$(uname -s | tr A-Z a-z).uri")
 # If eksctl-anywhere already exists, make a backup copy of it 
 # NOTE:  need to check whether the correct **version** already exists, then update, if needed.
@@ -28,6 +30,7 @@ curl $EKS_ANYWHERE_TARBALL_URL \
     | tar xz ./eksctl-anywhere
 sudo install -m 0755 ./eksctl-anywhere /usr/local/bin/eksctl-anywhere
 
+# Install kubectl CLI
 export OS="$(uname -s | tr A-Z a-z)" ARCH=$(test "$(uname -m)" = 'x86_64' && echo 'amd64' || echo 'arm64')
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/${OS}/${ARCH}/kubectl"
 sudo install -m 0755 ./kubectl /usr/local/bin/kubectl
