27abbb33f0281d7a954c25877f9f91cb978369be
cloudxabide <cloudxabide@gmail.com>
2024-02-21 18:11:51 -0500

    generic update

--- Scripts/Install_DHCP_Server.sh
+++ Scripts/Install_DHCP_Server.sh
@@ -9,7 +9,7 @@
 # Install the ISC DHCP Server package
 sudo apt install -y isc-dhcp-server
 
-cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.orig
+sudo cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.orig
 curl https://raw.githubusercontent.com/cloudxabide/kubernerdes/main/Files/etc_dhcp_dhcpd.conf | sudo tee /etc/dhcp/dhcpd.conf
 curl https://raw.githubusercontent.com/cloudxabide/kubernerdes/main/Files/etc_dhcp_dhcpd-hosts.conf | sudo tee /etc/dhcp/dhcpd-hosts.conf
 sudo systemctl restart isc-dhcp-server.service
@@ -17,3 +17,6 @@ sudo systemctl --no-pages status isc-dhcp-server.service
 
 journalctl -u isc-dhcp-server.service
 exit 0
+
+# Follow the log
+journalctl -f -u isc-dhcp-server.service
