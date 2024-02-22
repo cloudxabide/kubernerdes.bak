c7b9887229befdfc24338ac62c1d788a5bc6eca4
cloudxabide <cloudxabide@gmail.com>
2024-02-22 14:39:38 -0500

    Yupdates

--- Scripts/68_Random.sh
+++ Scripts/68_Random.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
 
+## Curated Packages List (work in progress)
+eksctl anywhere list packages --kube-version $(kubectl version -o json | jq -rj '.serverVersion|.major,".",.minor')
+
 ## Enable Metrics Server
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl get deployment metrics-server -n kube-system
 kubectl get events -n kube-system
 
-# Disable TLS for my metrics on my cluster
+### Disable TLS for my metrics on my cluster
 kubectl patch deployment metrics-server -n kube-system --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--kubelet-insecure-tls"}]'
-# NEED TO TEST THIS - IT *SHOULD* REPEAT UNTIL NO "0/1" IS FOUND
 while sleep 2; do kubectl get pods -n kube-system | grep ^metrics-server | grep "0/1" || break; done
 
-# Curated Packages List (work in progress)
-eksctl anywhere list packages --kube-version $(kubectl version -o json | jq -rj '.serverVersion|.major,".",.minor')
+## Enable Prometheus
+eksctl anywhere generate package prometheus --cluster $CLUSTER_NAME > prometheus.yaml
+# eksctl anywhere create packages -f prometheus.yaml
+cat << EOF1 | tee  prometheus-rep2-statefuleset.yaml
+---
+ apiVersion: packages.eks.amazonaws.com/v1alpha1
+ kind: Package
+ metadata:
+   name: generated-prometheus
+   namespace: eksa-packages-${CLUSTER_NAME}
+ spec:
+   packageName: prometheus
+   targetNamespace: observability
+   config: |
+     server:
+       replicaCount: 2
+       statefulSet:
+         enabled: true
+     serverFiles:
+       prometheus.yml:
+         scrape_configs:
+           - job_name: prometheus
+             static_configs:
+               - targets:
+                 - localhost:9090     
+EOF1
+kubectl create namespace observability
+eksctl anywhere create packages -f prometheus-rep2-statefuleset.yaml
+eksctl anywhere get packages --cluster $CLUSTER_NAME
 
-# ADOT
+## ADOT
 eksctl anywhere generate package adot --cluster $( kubectl config view --minify -o jsonpath='{.clusters[].name}') > adot.yaml
