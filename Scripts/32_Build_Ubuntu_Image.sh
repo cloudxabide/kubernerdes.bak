7af8d1dab6b666522b097087f946cd48cf8cf4d8
cloudxabide <cloudxabide@gmail.com>
2024-03-15 09:51:13 -0400

    Cleanup

--- Scripts/32_Build_Ubuntu_Image.sh
+++ Scripts/32_Build_Ubuntu_Image.sh
@@ -41,13 +41,11 @@ sudo apt upgrade -y
 sudo apt install jq make qemu-kvm libvirt-daemon-system libvirt-clients virtinst cpu-checker libguestfs-tools libosinfo-bin unzip -y
 sudo snap install yq
 sudo usermod -a -G kvm $USER
-newgrp kvm
 grep HostKeyAlgorithms /home/$USER/.ssh/config || { echo "HostKeyAlgorithms +ssh-rsa" >> /home/$USER/.ssh/config; }
 grep PubkeyAcceptedKeyTypes /home/$USER/.ssh/config || { echo "PubkeyAcceptedKeyTypes +ssh-rsa" >> /home/$USER/.ssh/config; } 
 chmod 0600 ${HOME}/.ssh/config
 
-# TEST (this should alleviate the need for the current user to logout/login to recognize the new group
-newgrp kvm   
+echo "If you just created the user - you need to logout/login to recognize being added to the kvm group"
 
 [ ! -f /usr/bin/make ] && sudo apt-get install make
 [ ! -f /usr/bin/jq ] && sudo apt install -y jq
@@ -58,22 +56,30 @@ python3 -m pip install --user ansible
 # If using Ubuntu 20.04 - you need to set python3.9 as the default
 # sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
 
-# Install Image Builder 
 #export EKSA_RELEASE_VERSION=v0.19.0 # Manually define version
 export EKSA_RELEASE_VERSION=$(curl -sL https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml | yq ".spec.latestVersion")
-cd /tmp
-BUNDLE_MANIFEST_URL=$(curl -s https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml | yq ".spec.releases[] | select(.version==\"$EKSA_RELEASE_VERSION\").bundleManifestUrl")
-IMAGEBUILDER_TARBALL_URI=$(curl -s $BUNDLE_MANIFEST_URL | yq ".spec.versionsBundles[0].eksD.imagebuilder.uri")
-curl -s $IMAGEBUILDER_TARBALL_URI | tar xz ./image-builder
-sudo install -m 0755 ./image-builder /usr/local/bin/image-builder
-cd -
+# Install Image Builder 
+[ ! -f /usr/local/bin/image-builder ] && {
+  cd /tmp
+  BUNDLE_MANIFEST_URL=$(curl -s https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml | yq ".spec.releases[] | select(.version==\"$EKSA_RELEASE_VERSION\").bundleManifestUrl")
+  IMAGEBUILDER_TARBALL_URI=$(curl -s $BUNDLE_MANIFEST_URL | yq ".spec.versionsBundles[0].eksD.imagebuilder.uri")
+  curl -s $IMAGEBUILDER_TARBALL_URI | tar xz ./image-builder
+  sudo install -m 0755 ./image-builder /usr/local/bin/image-builder
+  cd -
+}
 
 # Add Bash Completion (optional)
 mkdir ~/.bashrc.d
 image-builder completion bash > ~/.bashrc.d/image-builder
 
-## Cleanup
+## REPEATABLE SECTION HERE
+### Cleanup
 # rm -rf ${HOME}/eks-anywhere-build-tooling
+
+manual_versioning() {
+export EKSA_RELEASE_VERSION=v0.19.0 # Manually define version
+export EKSA_RELEASE_VERSION=$(curl -sL https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml | yq ".spec.latestVersion")
+}
 
 # Set some params
 export OS=ubuntu
@@ -81,8 +87,11 @@ export OS_VERSION=22.04
 export HYPERVISOR=baremetal
 export RELEASE_CHANNEL="1-29"
 
-echo EKSA_RELEASE_VERSION= $EKSA_RELEASE_VERSION
-#image-builder build --os ubuntu --hypervisor baremetal --release-channel 1-28
-image-builder build --os $OS --os-version $OS_VERSION --hypervisor $HYPERVISOR --release-channel $RELEASE_CHANNEL
+EKSA_RELEASE_VERSION=v0.18.0; RELEASE_CHANNEL="1-28"
+EKSA_RELEASE_VERSION=v0.19.0; RELEASE_CHANNEL="1-29"
+
+echo EKSA_RELEASE_VERSION: $EKSA_RELEASE_VERSION
+echo "image-builder build --os $OS --os-version $OS_VERSION --hypervisor $HYPERVISOR --release-channel $RELEASE_CHANNEL --eksa-release $EKSA_RELEASE_VERSION"
+image-builder build --os $OS --os-version $OS_VERSION --hypervisor $HYPERVISOR --release-channel $RELEASE_CHANNEL --eksa-release $EKSA_RELEASE_VERSION
 
 exit 0
