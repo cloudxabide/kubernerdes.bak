97a4eb32db96bd2efc36bc07378385266433c47f
cloudxabide <cloudxabide@gmail.com>
2024-03-18 15:45:20 -0400

    New file for tertiary domain

--- Scripts/61_Ingress_Router.sh
+++ Scripts/61_Ingress_Router.sh
@@ -26,7 +26,6 @@ spec:
     IPAddressPools:
       - name: default
         addresses:
-          - 10.220.0.93/32
           - 10.10.13.1-10.10.13.255
     L2Advertisements:
       - ipAddressPools:
@@ -36,9 +35,64 @@ kubectl create namespace metallb-system
 eksctl anywhere create packages -f metallb-config.yaml
 eksctl anywhere get packages --cluster $CLUSTER_NAME
 
-kubectl apply -f "https://anywhere.eks.amazonaws.com/manifests/hello-eks-a.yaml"
+kubectl create namespace hello-world
+kubectl apply -f "https://raw.githubusercontent.com/cloudxabide/kubernerdes/main/Files/hello-world/hello-world-eks-a-with-lb.yaml" -n hello-world
 sleep 2
-kubectl get pods -l app=hello-eks-a
+kubectl get pods -l app=hello-eks-a -n hello-world
 kubectl logs -l app=hello-eks-a
+kubectl get all -n hello-world
+kubectl delete ns hello-world
 
-exit 0
+## Deploy Emissary
+eksa-packages-emissary() {
+mkdir ~/DevOps/eksa/latest/emissary; cd $_
+eksctl anywhere generate package emissary --cluster $CLUSTER_NAME > generated-emissary.yaml
+kubectl create namespace emissary-system
+cat << EOF1 | tee emissary.yaml
+---
+apiVersion: packages.eks.amazonaws.com/v1alpha1
+kind: Package
+metadata:
+  name: emissary
+  namespace: eksa-packages-$CLUSTER_NAME
+spec:
+  packageName: emissary
+EOF1
+
+eksctl anywhere create packages -f emissary.yaml 
+eksctl anywhere get packages --cluster $CLUSTER_NAME | grep emiss
+
+# eksctl anywhere delete package emissary --cluster $CLUSTER_NAME
+# eksctl anywhere delete package emissary-crds --cluster $CLUSTER_NAME
+}
+
+emissary_OSS() {
+
+exit dd the Repo:
+helm repo add datawire https://app.getambassador.io
+helm repo update
+ 
+# Create Namespace and Install:
+kubectl create namespace emissary && \
+kubectl apply -f https://app.getambassador.io/yaml/emissary/3.9.1/emissary-crds.yaml
+ 
+kubectl wait --timeout=90s --for=condition=available deployment emissary-apiext -n emissary-system
+ 
+helm install emissary-ingress --namespace emissary datawire/emissary-ingress && \
+kubectl -n emissary wait --for condition=available --timeout=90s deploy -lapp.kubernetes.io/instance=emissary-ingress
+
+kubectl get svc -w  --namespace emissary emissary-ingress
+
+# https://www.getambassador.io/docs/emissary/latest/topics/running/host-crd
+cat << EOF1 | tee hello-world.yaml
+---
+apiVersion: getambassador.io/v3alpha1
+kind: Host
+metadata:
+  name: hello-world
+spec:
+  hostname: *.apps.kubernerdes.lab
+EOF1
+
+
+}
