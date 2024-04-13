94612d6c4cf2a1b242024625f92ab8732c281d01
cloudxabide <cloudxabide@gmail.com>
2024-04-12 21:31:37 -0400

    generic update

---
+++ Scripts/53_Install_NFS-CSI.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Purpose:  install csi-driver-nfs
+#    Note:  CSI is the current standard and therefore I am focused on using it
+#  Status:  Working
+
+#  https://github.com/kubernetes-csi/csi-driver-nfs
+
+cd ~/eksa/$CLUSTER_NAME/latest
+
+# Install using kubectl
+curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/v4.6.0/deploy/install-driver.sh | bash -s v4.6.0 --
+curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/v4.6.0/deploy/uninstall-driver.sh | bash -s v4.6.0 --
+
+# Install using Helm (prefered, I think)
+helm repo add csi-driver-nfs https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts
+helm install csi-driver-nfs csi-driver-nfs/csi-driver-nfs --namespace kube-system --version v4.6.0
+
+kubectl --namespace=kube-system get pods --selector="app.kubernetes.io/instance=csi-driver-nfs"
+
+
+cat << EOF1 | tee nfs-csi-sc.yaml
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: nfs-csi
+provisioner: nfs.csi.k8s.io
+parameters:
+  server: 10.10.10.19
+  share: /mnt/SSD-1TB
+  # csi.storage.k8s.io/provisioner-secret is only needed for providing mountOptions in DeleteVolume
+  # csi.storage.k8s.io/provisioner-secret-name: "mount-options"
+  # csi.storage.k8s.io/provisioner-secret-namespace: "default"
+reclaimPolicy: Delete
+volumeBindingMode: Immediate
+mountOptions:
+  - nfsvers=4.1
+EOF1
+kubectl create -f nfs-csi-sc.yaml
+kubectl get sc nfs-csi
+kubectl create -f https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/deploy/example/pvc-nfs-csi-dynamic.yaml
+kubectl get pv,pvc
+
+exit 0
+
+# create PV
+kubectl create -f https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/deploy/example/pv-nfs-csi.yaml
+
+# create PVC
+kubectl create -f https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/deploy/example/pvc-nfs-csi-static.yaml
+
+kubectl create -f https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/deploy/example/deployment.yaml
