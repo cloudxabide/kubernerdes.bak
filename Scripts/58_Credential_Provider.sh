8726efbf129119c6a577e586a947609776e74d1c
cloudxabide <cloudxabide@gmail.com>
2024-04-21 11:32:46 -0400

    generic update

--- Scripts/58_Credential_Provider.sh
+++ Scripts/58_Credential_Provider.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#     Purpose:
+#        Date:
+#      Status: 
+# Assumptions:
+#        Todo:
+#       Notes:
+
 cd ~/DevOps/eksa/latest/ 
 kubectl get secret -n eksa-system ${CLUSTER_NAME}-ca -o yaml | yq '.data."tls.crt"' | base64 -d > $CLUSTER_NAME.pem
 openssl x509 -in $CLUSTER_NAME.pem -text
