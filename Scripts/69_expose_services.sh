225ad651f5083963f51c611d9affa317b85bf911
cloudxabide <cloudxabide@gmail.com>
2024-04-21 10:04:27 -0400

    generic update

--- Scripts/69_expose_services.sh
+++ Scripts/69_expose_services.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Status:  Need to work on this and figure out how to assign pre-determined addresses to service
+#            Likely do an NSLOOKUP to get the IP from DNS, then assign
 
 # Prometheus
 kubectl patch svc generated-prometheus-server -n observability -p '{"spec": {"type": "LoadBalancer"}}'
 kubectl describe svc generated-prometheus-server -n observability
 # http://generated-prometheus-server.observability:9090
 
-# Grafana
+t
+ Grafana
 kubectl patch svc my-grafana -n monitoring -p '{"spec": {"type": "LoadBalancer"}}'
 kubectl describe svc my-grafana -n monitoring
 # 315 1860
