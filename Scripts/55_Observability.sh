5979f9afaca7b4959dcd9574a8a406c9e62a0231
James Radtke <cloudxabide@gmail.com>
2024-05-15 15:49:23 -0400

    generic update

--- Scripts/55_Observability.sh
+++ Scripts/55_Observability.sh
@@ -2,9 +2,111 @@
 
 #     Purpose: To install Prometheus/Grafana
 #        Date: 2024-04-24
-#      Status: Unknown - believed to be GTG
+#      Status: Work-In-Progress - converting to OSS
 # Assumptions:
 #        Todo: Update this procedure to use OSS versions of Prometheus and Grafana.  (Create helm charts?)
+
+# https://github.com/prometheus-operator/prometheus-operator
+
+# https://github.com/prometheus-operator/kube-prometheus
+
+
+# Create the namespace and CRDs, and then wait for them to be available before creating the remaining resources
+# Note that due to some CRD size we are using kubectl server-side apply feature which is generally available since kubernetes 1.22.
+# If you are using previous kubernetes versions this feature may not be available and you would need to use kubectl create instead.
+
+git clone https://github.com/prometheus-operator/kube-prometheus.git
+cd kube-prometheus/
+#
+kubectl apply --server-side -f manifests/setup
+kubectl wait \
+	--for condition=Established \
+	--all CustomResourceDefinition \
+	--namespace=monitoring
+kubectl apply -f manifests/
+while sleep 2; do kubectl get all -n monitoring | egrep 'ContainerCreating|Init' || break; done
+
+
+
+
+
+kubectl config set-context --current --namespace=default
+kubectl create -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/master/bundle.yaml
+kubectl get deploy -n default
+mkdir operator_k8s
+cd operator_k8s
+
+cat << EOF1 | tee prom_rbac.yaml
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: prometheus
+---
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRole
+metadata:
+  name: prometheus
+rules:
+- apiGroups: [""]
+  resources:
+  - nodes
+  - nodes/metrics
+  - services
+  - endpoints
+  - pods
+  verbs: ["get", "list", "watch"]
+- apiGroups: [""]
+  resources:
+  - configmaps
+  verbs: ["get"]
+- apiGroups:
+  - networking.k8s.io
+  resources:
+  - ingresses
+  verbs: ["get", "list", "watch"]
+- nonResourceURLs: ["/metrics"]
+  verbs: ["get"]
+---
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: prometheus
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: prometheus
+subjects:
+- kind: ServiceAccount
+  name: prometheus
+  namespace: default
+EOF1
+kubectl apply -f prom_rbac.yaml
+
+
+
+clean_up() {
+for n in $(kubectl get namespaces -o jsonpath={..metadata.name}); do
+  kubectl delete --all --namespace=$n prometheus,servicemonitor,podmonitor,alertmanager
+done
+kubectl delete -f bundle.yaml
+for n in $(kubectl get namespaces -o jsonpath={..metadata.name}); do
+  kubectl delete --ignore-not-found --namespace=$n service prometheus-operated alertmanager-operated
+done
+
+kubectl delete --ignore-not-found customresourcedefinitions \
+  prometheuses.monitoring.coreos.com \
+  servicemonitors.monitoring.coreos.com \
+  podmonitors.monitoring.coreos.com \
+  alertmanagers.monitoring.coreos.com \
+  prometheusrules.monitoring.coreos.com
+
+
+
+
+
+
+
+
 
 
 
